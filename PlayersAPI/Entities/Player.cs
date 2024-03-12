0ef384fa84fa147cc91e763f28070f07fbf70cfc
RamonAPereira <ramonpereiranzd@gmail.com>
2024-03-12 00:16:26 -0300

    Player Entity

---
+++ PlayersAPI/Entities/Player.cs
@@ -0,0 +1,13 @@
+﻿using PlayersAPI.Enums;
+
+namespace PlayersAPI.Entities
+{
+    public class Player
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public int Telefone { get; set; }
+        public required string Nickname { get; set; }
+        public HeroGroup MyProperty { get; set; }
+    }
+}
